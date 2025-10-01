6c9821183ed49f76595b915eb6dbd2bbe9b69fd1
julian <julian_alb.flor@uao.edu.co>
2025-10-01 10:38:43 -0500

    este es otro programa 11

--- src/main/java/com/mycompany/herencianetbeans/seg.java
+++ src/main/java/com/mycompany/herencianetbeans/seg.java
@@ -1,18 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.herencianetbeans;
 
 /**
  *
  * @author eljul
  */
-public class HerenciaNetbeans {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello  petro!");
-
-    }
+public class seg {
+    
 }
